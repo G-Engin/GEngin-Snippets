c89f3c7b3eb04e791f24226f4943a1e44fba58a0
Ronildo N. Souza <ronildo_rns@hotmail.com>
2017-07-13 22:19:37 -0300

    Completions for enum mode debug function _GEng_SetDebug

--- out/src/completions/Global_Enums.js
+++ out/src/completions/Global_Enums.js
@@ -21,6 +21,22 @@ var items = [{
     {
         label: '$GEng_Origin_BR',
         documentation: 'Bottom Right\nFile GEng_Sprite.au3'
+    },
+    {
+        label: '$GEng_Debug_Pens',
+        documentation: 'Use in _GEng_SetDebug($mode)\nFile GEng_System.au3'
+    },
+    {
+        label: '$GEng_Debug_Sprites',
+        documentation: 'Use in _GEng_SetDebug($mode)\nFile GEng_System.au3'
+    },
+    {
+        label: '$GEng_Debug_Vectors',
+        documentation: 'Use in _GEng_SetDebug($mode)\nFile GEng_System.au3'
+    },
+    {
+        label: '$GEng_Debug_Collisions',
+        documentation: 'Use in _GEng_SetDebug($mode)\nFile GEng_System.au3'
     }
 ]
 
